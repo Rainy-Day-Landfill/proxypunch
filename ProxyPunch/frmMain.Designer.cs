a5704b14da6e64a7ad5eea4bf90ede270bdb0c31
cmpunches <punches.chris@gmail.com>
2015-05-03 05:19:11 -0400

    Checkin

--- ProxyPunch/frmMain.Designer.cs
+++ ProxyPunch/frmMain.Designer.cs
@@ -28,110 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.btnBind = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtSocksIP = new System.Windows.Forms.TextBox();
-            this.lblSocksPort = new System.Windows.Forms.Label();
-            this.txtSocksPort = new System.Windows.Forms.TextBox();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtLocalPort = new System.Windows.Forms.TextBox();
-            this.txtOutput = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // btnBind
-            // 
-            this.btnBind.Location = new System.Drawing.Point(511, 10);
-            this.btnBind.Name = "btnBind";
-            this.btnBind.Size = new System.Drawing.Size(75, 23);
-            this.btnBind.TabIndex = 0;
-            this.btnBind.Text = "Bind";
-            this.btnBind.UseVisualStyleBackColor = true;
-            this.btnBind.Click += new System.EventHandler(this.btnBind_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Socks IP";
-            // 
-            // txtSocksIP
-            // 
-            this.txtSocksIP.Location = new System.Drawing.Point(64, 12);
-            this.txtSocksIP.Name = "txtSocksIP";
-            this.txtSocksIP.Size = new System.Drawing.Size(100, 20);
-            this.txtSocksIP.TabIndex = 2;
-            // 
-            // lblSocksPort
-            // 
-            this.lblSocksPort.AutoSize = true;
-            this.lblSocksPort.Location = new System.Drawing.Point(170, 15);
-            this.lblSocksPort.Name = "lblSocksPort";
-            this.lblSocksPort.Size = new System.Drawing.Size(59, 13);
-            this.lblSocksPort.TabIndex = 3;
-            this.lblSocksPort.Text = "Socks Port";
-            // 
-            // txtSocksPort
-            // 
-            this.txtSocksPort.Location = new System.Drawing.Point(235, 12);
-            this.txtSocksPort.Name = "txtSocksPort";
-            this.txtSocksPort.Size = new System.Drawing.Size(65, 20);
-            this.txtSocksPort.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(306, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "to 127.0.0.1:";
-            // 
-            // txtLocalPort
-            // 
-            this.txtLocalPort.Location = new System.Drawing.Point(379, 12);
-            this.txtLocalPort.Name = "txtLocalPort";
-            this.txtLocalPort.Size = new System.Drawing.Size(65, 20);
-            this.txtLocalPort.TabIndex = 6;
-            // 
-            // txtOutput
-            // 
-            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtOutput.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOutput.Location = new System.Drawing.Point(13, 39);
-            this.txtOutput.Multiline = true;
-            this.txtOutput.Name = "txtOutput";
-            this.txtOutput.ReadOnly = true;
-            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.Size = new System.Drawing.Size(573, 210);
-            this.txtOutput.TabIndex = 7;
-            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
-            // 
-            // frmMain
-            // 
-            this.AcceptButton = this.btnBind;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 261);
-            this.Controls.Add(this.txtOutput);
-            this.Controls.Add(this.txtLocalPort);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtSocksPort);
-            this.Controls.Add(this.lblSocksPort);
-            this.Controls.Add(this.txtSocksIP);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnBind);
-            this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(614, 300);
-            this.MinimumSize = new System.Drawing.Size(614, 300);
-            this.Name = "frmMain";
-            this.Text = "ProxyPunch by SILO GROUP";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			this.btnBind = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtSocksIP = new System.Windows.Forms.TextBox();
+			this.lblSocksPort = new System.Windows.Forms.Label();
+			this.txtSocksPort = new System.Windows.Forms.TextBox();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtLocalPort = new System.Windows.Forms.TextBox();
+			this.txtOutput = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnBind
+			// 
+			this.btnBind.Location = new System.Drawing.Point(511, 10);
+			this.btnBind.Name = "btnBind";
+			this.btnBind.Size = new System.Drawing.Size(75, 23);
+			this.btnBind.TabIndex = 0;
+			this.btnBind.Text = "Bind";
+			this.btnBind.UseVisualStyleBackColor = true;
+			this.btnBind.Click += new System.EventHandler(this.btnBind_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(50, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Socks IP";
+			// 
+			// txtSocksIP
+			// 
+			this.txtSocksIP.Location = new System.Drawing.Point(64, 12);
+			this.txtSocksIP.Name = "txtSocksIP";
+			this.txtSocksIP.Size = new System.Drawing.Size(100, 20);
+			this.txtSocksIP.TabIndex = 2;
+			// 
+			// lblSocksPort
+			// 
+			this.lblSocksPort.AutoSize = true;
+			this.lblSocksPort.Location = new System.Drawing.Point(170, 15);
+			this.lblSocksPort.Name = "lblSocksPort";
+			this.lblSocksPort.Size = new System.Drawing.Size(59, 13);
+			this.lblSocksPort.TabIndex = 3;
+			this.lblSocksPort.Text = "Socks Port";
+			// 
+			// txtSocksPort
+			// 
+			this.txtSocksPort.Location = new System.Drawing.Point(235, 12);
+			this.txtSocksPort.Name = "txtSocksPort";
+			this.txtSocksPort.Size = new System.Drawing.Size(65, 20);
+			this.txtSocksPort.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(306, 15);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "to 127.0.0.1:";
+			// 
+			// txtLocalPort
+			// 
+			this.txtLocalPort.Location = new System.Drawing.Point(379, 12);
+			this.txtLocalPort.Name = "txtLocalPort";
+			this.txtLocalPort.Size = new System.Drawing.Size(65, 20);
+			this.txtLocalPort.TabIndex = 6;
+			// 
+			// txtOutput
+			// 
+			this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtOutput.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtOutput.Location = new System.Drawing.Point(13, 39);
+			this.txtOutput.Multiline = true;
+			this.txtOutput.Name = "txtOutput";
+			this.txtOutput.ReadOnly = true;
+			this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtOutput.Size = new System.Drawing.Size(573, 210);
+			this.txtOutput.TabIndex = 7;
+			this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+			// 
+			// frmMain
+			// 
+			this.AcceptButton = this.btnBind;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(598, 261);
+			this.Controls.Add(this.txtOutput);
+			this.Controls.Add(this.txtLocalPort);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtSocksPort);
+			this.Controls.Add(this.lblSocksPort);
+			this.Controls.Add(this.txtSocksIP);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnBind);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(614, 300);
+			this.MinimumSize = new System.Drawing.Size(614, 300);
+			this.Name = "frmMain";
+			this.Text = "ProxyPunch by SILO GROUP";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -144,8 +145,8 @@
         private System.Windows.Forms.TextBox txtSocksPort;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtLocalPort;
-        private System.Windows.Forms.TextBox txtOutput;
+		private System.Windows.Forms.TextBox txtLocalPort;
+		public System.Windows.Forms.TextBox txtOutput;
     }
 }
 
--- ProxyPunch/frmMain.cs
+++ ProxyPunch/frmMain.cs
@@ -9,103 +9,44 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
 
 namespace ProxyPunch
 {
 	public partial class frmMain : Form
 	{
+		public string ProxyEndpoint
+		{
+			get { return txtSocksIP.Text; }
+		}
+
+		public string ProxyPort
+		{
+			get { return txtSocksPort.Text; }
+		}
+
+		public string LocalPort
+		{
+			get { return txtLocalPort.Text; }
+		}
+
+		public string LogLine
+		{
+			set { txtOutput.AppendText(value + Environment.NewLine); }
+		}
+
+		public static frmMain _formHandle;
+
 		public frmMain()
 		{
 			InitializeComponent();
 			notifyln("UI Initialized.");
+			_formHandle = this;
+
 			notifyln("Ready to bind a SOCKS IP:PORT to 127.0.0.1:PORT");
 		}
 
-		private void notify(string input)
-		{
-			// txtOutput.Text += input;
-			txtOutput.AppendText(input);
-		}
-
-		private void CreateListener()
-		{
-
-		}
-
-
-		private void ServerAction(string strSocksIP, int socksPort, int localPort)
-		{
-			Reactor.Loop.Start(System.Threading.SynchronizationContext.Current);
-
-			notifyln("Ready for local connection.");
-			notifyln(String.Format("Please point your application's proxy settings to 127.0.0.1:{0}", localPort));
-
-
-			Reactor.Tcp.Server.Create(LocalListener =>
-			{
-				Reactor.Tcp.Socket ProxyClient = Reactor.Tcp.Socket.Create(strSocksIP, socksPort);
-				notifyln("Attempting to connect to proxy server...");
-
-				LocalListener.OnData += (data) =>
-				{
-					notifyln(data.ToString(Encoding.ASCII));
-				};
-
-				LocalListener.OnConnect += () =>
-				{
-					notifyln(String.Format("Received connection on 127.0.0.1:{0}", localPort));
-				};
-
-				LocalListener.OnError += (error) =>
-				{
-					ProxyClient.End();
-					notifyln("Connection on local listener dropped:");
-					notifyln(error.Message);
-				};
-
-				LocalListener.OnEnd += () =>
-				{
-					ServerAction(strSocksIP, socksPort, localPort);
-
-					LocalListener.End();
-					ProxyClient.End();
-				};
-
-				ProxyClient.OnEnd += () =>
-				{
-					LocalListener.End();
-					notifyln("Connection to proxy server dropped (Proxy).");
-
-					LocalListener.End();
-					ProxyClient.End();
-				};
-
-				// route local events to the socket.
-				ProxyClient.OnData += (data) =>
-				{
-					notifyln(data.ToString(Encoding.ASCII));
-					LocalListener.Write(data);
-				};
-
-				ProxyClient.OnError += (error) =>
-				{
-					notifyln(String.Format("Connection to proxy server dropped:\n\r{0}\n\r", error.Message));
-					ProxyClient.End();
-					LocalListener.End();
-				};
-
-				ProxyClient.OnConnect += () =>
-				{
-					notifyln(String.Format("Connection established with proxy server {0}:{1}.", strSocksIP, socksPort));
-					LocalListener.OnData += (data) =>
-					{
-						ProxyClient.Write(data);
-					};
-				};
-			}).Listen(localPort);
-		}
-
-		private void notifyln(string input)
+		public void notifyln(string input)
 		{
 			txtOutput.AppendText(input + Environment.NewLine);
 		}
@@ -127,8 +68,16 @@ namespace ProxyPunch
 
 		private void btnBind_Click(object sender, EventArgs e)
 		{
-			Reactor.Loop.Stop();
-			ServerAction(txtSocksIP.Text, Convert.ToInt32(txtSocksPort.Text), Convert.ToInt32(txtLocalPort.Text));
+			if ( txtSocksIP.Text.Equals("") | txtSocksPort.Text.Equals("") | txtLocalPort.Text.Equals("") )
+			{
+				notifyln("You've entered incorrect values.  Please try again.");
+			}
+			else
+			{
+				// ServerAction(txtSocksIP.Text, Convert.ToInt32(txtSocksPort.Text), Convert.ToInt32(txtLocalPort.Text));
+				TSocket ThisSocket = new TSocket(_formHandle);
+				ThisSocket.CreateListener(1234);
+			}
 		}
 
 		private void txtOutput_TextChanged(object sender, EventArgs e)
@@ -138,5 +87,89 @@ namespace ProxyPunch
 			txtOutput.ScrollToCaret();
 			txtOutput.Refresh();
 		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+
+	public class TSocket
+	{
+
+		public static IPAddress LOCALHOST = IPAddress.Parse("127.0.0.1");
+
+		public static ManualResetEvent tcpClientConnected = new ManualResetEvent(false);
+
+		// using the class constructor to interface with the form fields
+
+		// create an instance of the form?  how does this work?
+		private readonly frmMain form;
+
+		public TSocket(frmMain form)
+		{
+			this.form = form;
+		}
+
+		public void log (string entry)
+		{
+			form.LogLine = entry;
+		}
+
+		public void CreateListener(int ListenerPort)
+		{
+			log( "Creating a listener socket..." );
+			TcpListener LocalListener = new TcpListener(LOCALHOST, ListenerPort);
+			log( "Listener socket created." );
+
+			log( "Starting socket..." );
+			LocalListener.Start();
+
+			DoBeginAcceptTcpClient(LocalListener);
+
+			// Byte[] bytes = new Byte[256];
+			// String data = null;
+
+		}
+
+		public void DoBeginAcceptTcpClient(TcpListener listener)
+		{
+			// Set the event to nonsignaled state.
+			tcpClientConnected.Reset();
+
+			// Start to listen for connections from a client.
+			log( "Waiting for a connection..." );
+
+			// Accept the connection.
+			// BeginAcceptSocket() creates the accepted socket.
+			listener.BeginAcceptTcpClient( new AsyncCallback( DoAcceptTcpClientCallback ), listener );
+			log( String.Format( "Listening on {0}:{1}", LOCALHOST, ( (IPEndPoint)listener.LocalEndpoint ).Port.ToString() ) );
+
+			// Wait until a connection is made and processed before
+			// continuing.
+			tcpClientConnected.WaitOne();
+		}
+
+		// Process the client connection.
+		public void DoAcceptTcpClientCallback(IAsyncResult ar)
+		{
+			// Get the listener that handles the client request.
+			TcpListener listener = (TcpListener)ar.AsyncState;
+
+			// End the operation and display the received data on
+			// the console.
+			TcpClient client = listener.EndAcceptTcpClient(ar);
+
+			// Process the connection here. (Add the client to a
+			// server table, read data, etc.)
+			form.LogLine = "Client connected completed";
+
+			// Signal the calling thread to continue.
+			tcpClientConnected.Set();
+		}
+
+
+
+
 	}
 }
